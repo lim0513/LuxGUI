de402d3f68cbe9f3a76daf821b592165618f6e13
lim <lim@keepourfaith.com>
2022-10-19 14:10:30 +0800

    Try to add multiple languages

--- Lux_GUI/Properties/Resources.Designer.cs
+++ Lux_GUI/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace Lux_GUI.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Lux_GUI.Properties {
         ///   返回此类使用的缓存的 ResourceManager 实例。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lux_GUI.Properties.Resources", typeof(Resources).Assembly);
@@ -51,12 +51,48 @@ namespace Lux_GUI.Properties {
         ///   使用此强类型资源类的所有资源查找执行重写。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 解析 的本地化字符串。
+        /// </summary>
+        public static string BtnParse {
+            get {
+                return ResourceManager.GetString("BtnParse", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 播放列表 的本地化字符串。
+        /// </summary>
+        public static string CbPlayLists {
+            get {
+                return ResourceManager.GetString("CbPlayLists", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 下载地址 的本地化字符串。
+        /// </summary>
+        public static string LbDownloadURL {
+            get {
+                return ResourceManager.GetString("LbDownloadURL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 解析资源中 的本地化字符串。
+        /// </summary>
+        public static string LbParsing {
+            get {
+                return ResourceManager.GetString("LbParsing", resourceCulture);
             }
         }
     }
